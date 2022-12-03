36da42b6d066dab32f8bd01ae925201991d5c281
SourabhBidikar <bidikarsourabh@gmail.com>
2022-12-03 16:18:53 +0530

    override codes

---
+++ core-java/src/com/sourabh/oops/DemoPolymorpCase1.java
@@ -0,0 +1,43 @@
+
+public class DemoPolymorpCase1 {
+
+	public static void main(String[] args) {
+
+		DemoPolymorpCase1 dpc = new DemoPolymorpCase1();
+		
+		dpc.show('a');
+		
+		dpc.show(5,6,9); //for varargs
+		dpc.show(); //for varargs
+		
+		/*
+		 * here we passed character as arg still it called
+		 * method with int as argument
+		 * THIS IS CALLED AUTOMATIC PROMOTION.
+		 */
+
+	}
+
+	void show(int a)
+	{
+		System.out.println("int method");
+	}
+
+
+	void show(String str)
+	{
+		System.out.println("string method");
+	} 
+	
+////////////////////////////////////
+	
+	//variable args
+	void show(int ... a)
+	{
+		System.out.println("arg method");
+	}
+
+	
+
+}
+
---
+++ core-java/src/com/sourabh/oops/DemoPolymorpOverLoading1.java
@@ -0,0 +1,48 @@
+
+/*
+ * Compile time or static or overloading
+ */
+
+
+public class DemoPolymorpOverLoading1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	StudentName s = new StudentName("sourabh","Bidikar");
+	
+	s.printName(s.firstname);
+	
+	System.out.println();
+	
+	s.printName(s.firstname,s.lastname);
+	
+	}
+	
+	
+
+
+
+}
+
+
+class StudentName{
+	String firstname;
+	String lastname;
+	
+	
+	public StudentName(String firstname, String lastname) {
+		super();
+		this.firstname = firstname;
+		this.lastname = lastname;
+	}
+
+	void printName(String firstname) { 
+		System.out.println("Name is: "+firstname);
+	}
+	
+	void printName(String firstname,String lastname) { 
+		System.out.println("Name is: "+firstname+" "+lastname);
+	}
+	
+}
